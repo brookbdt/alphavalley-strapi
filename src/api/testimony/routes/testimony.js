bac4126fd7258a3d78ad90fa4a66cdc42a2ccfb0
Brook <brook@kelaltech.com>
2022-06-14 11:17:01 +0300

    v4

--- src/api/testimony/routes/testimony.js
+++ src/api/testimony/routes/testimony.js
@@ -1,34 +1,34 @@
 module.exports = {
   routes: [
     {
-      method: 'GET',
-      path: '/testimonies',
-      handler: 'testimony.find',
-      config: { policies: [] }
+      method: "GET",
+      path: "/testimonies",
+      handler: "testimony.find",
+      config: { policies: [] },
     },
     {
-      method: 'GET',
-      path: '/testimonies/:id',
-      handler: 'testimony.findOne',
-      config: { policies: [] }
+      method: "GET",
+      path: "/testimonies/:id",
+      handler: "testimony.findOne",
+      config: { policies: [] },
     },
     {
-      method: 'POST',
-      path: '/testimonies',
-      handler: 'testimony.create',
-      config: { policies: [] }
+      method: "POST",
+      path: "/testimonies",
+      handler: "testimony.create",
+      config: { policies: [] },
     },
     {
-      method: 'PUT',
-      path: '/testimonies/:id',
-      handler: 'testimony.update',
-      config: { policies: [] }
+      method: "PUT",
+      path: "/testimonies/:id",
+      handler: "testimony.update",
+      config: { policies: [] },
     },
     {
-      method: 'DELETE',
-      path: '/testimonies/:id',
-      handler: 'testimony.delete',
-      config: { policies: [] }
-    }
-  ]
-}
+      method: "DELETE",
+      path: "/testimonies/:id",
+      handler: "testimony.delete",
+      config: { policies: [] },
+    },
+  ],
+};
--- v3/extensions/users-permissions/config/jwt.js
+++ v3/extensions/users-permissions/config/jwt.js
@@ -1,3 +1,3 @@
 module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '718e8b5c-078a-4182-9319-a2e8b3a26ae7'
+  jwtSecret: process.env.JWT_SECRET || "718e8b5c-078a-4182-9319-a2e8b3a26ae7",
 };
